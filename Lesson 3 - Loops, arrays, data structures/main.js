730dcab0bf03a8a9e7a1d38553a51d4f575f0217
beardm4n <dmitriykorovko@gmail.com>
2019-09-05 20:21:58 +0300

    Decomposed the first task. Updated the second task: added two methods - forEach and reduce

--- Lesson 3 - Loops, arrays, data structures/main.js
+++ Lesson 3 - Loops, arrays, data structures/main.js
@@ -11,20 +11,24 @@ let min = +prompt('Input the number №1'),
     finalArr = [],
     i = 0;
 
-while (min < max) {
-   // по договоренности сообщества было принято не брать в расчет числа 0 и 1,
-   // для сравнения простых чисел, поэтому проверку будем осуществлять с 2-х
-   let  numberToCompare = 2;
-   //проверка: делится ли число на 1 или нет
-   while (numberToCompare < min) {
-      if (min % numberToCompare === 0) break;
+function isPrime(num) {
+   if (num === 0 || num === 1) return false;
+
+   let numberToCompare = 2;
+
+   while (numberToCompare < num) {
+      if (num % numberToCompare === 0) {
+         return false;
+      }
+
       numberToCompare++;
    }
-   //проверка: равно ли число самому себе
-   if (numberToCompare === min) {
-      finalArr[i] = min;
-      i++;
-   }
+
+   return true;
+}
+
+while (min < max) {
+   if (isPrime(min)) console.log(min);
 
    min++;
 }
@@ -39,23 +43,21 @@ Task #2
 console.log('=== TASK #2 ===');
 
 let cart = [
-    ['Тарелки', '15', '350'],
-    ['Чашки', '24', '175'],
-    ['Ложки', '80', '89'],
-    ['Кастрюли', '5', '950'],
+    ['Тарелки', '5', '350'],
+    ['Чашки', '4', '175'],
+    ['Ложки', '6', '89'],
+    ['Кастрюли', '9', '950'],
     ['Сковородки', '11', '1386'],
-    ['Ножи', '87', '2150'],
-    ['Вилки', '80', '75']
+    ['Ножи', '12', '2150'],
+    ['Вилки', '12', '75']
 ];
 
 function countBasketPrice(arr) {
-   let sum = 0;
+   let sum = [];
+   
+   arr.forEach((item, index) => sum[index] =  item[1] * item[2]);
 
-   for (let i = 0; i < arr.length; i++) {
-      sum = sum + parseInt(arr[i][2], 10);
-   }
-
-   return sum;
+   return sum.reduce((acc,current) => acc + current, 0);
 }
 
 console.log(`Total prise: ${countBasketPrice(cart)} RUB`);
@@ -78,8 +80,8 @@ Task #4
 */
 console.log('=== TASK #4 ===');
 
-let num1 = prompt('Inter the number #1'),
-    num2 = prompt('Enter the number #2'),
+let num1 = +prompt('Enter the number #1'),
+    num2 = +prompt('Enter the number #2'),
     symbol = '',
     numberToCompare = num1;
 

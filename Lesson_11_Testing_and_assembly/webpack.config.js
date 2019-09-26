f0838bc50b12a350cdb7c956e1f25cfbbdb2e457
beardm4n <dmitriykorovko@gmail.com>
2019-09-26 21:05:59 +0300

    completed the task

---
+++ Lesson_11_Testing_and_assembly/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+   output: {
+     path: path.resolve(__dirname, 'dist'),
+     filename: 'bundle.js',
+   },
+   module: {
+     rules: [
+        {
+           test: /\.js$/,
+           exclude: /node_modules/,
+           use: {
+              loader: 'babel-loader',
+           },
+        },
+        {
+           test: /\.css$/,
+           use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+     ]
+   },
+   plugins: [
+       new MiniCssExtractPlugin({
+          filename: 'style.css',
+       }),
+       new HtmlWebpackPlugin({
+          template: path.resolve(__dirname, 'src', 'index.html'),
+          filename: 'index.html',
+       })
+   ]
+};
